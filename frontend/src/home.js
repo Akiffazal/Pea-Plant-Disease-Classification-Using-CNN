c441915b8d8daf91d6404f9923fe6aaadeac9eba
Akif Fazal <akiffazal007@gmail.com>
2025-11-11 02:02:57 +0500

    Update frontend files

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -9,7 +9,6 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import { Paper, CardActionArea, CardMedia, Grid, TableContainer, Table, TableBody, TableHead, TableRow, TableCell, Button, CircularProgress } from "@material-ui/core";
-import cblogo from "./cblogo.PNG";
 import image from "./bg.png";
 import { DropzoneArea } from 'material-ui-dropzone';
 import { common } from '@material-ui/core/colors';
@@ -184,7 +183,6 @@ export const ImageUpload = () => {
             Pea Plant Disease Detection
           </Typography>
           <div className={classes.grow} />
-          <Avatar src={cblogo}></Avatar>
         </Toolbar>
       </AppBar>
       <Container maxWidth={false} className={classes.mainContainer} disableGutters={true}>
